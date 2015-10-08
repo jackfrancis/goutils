cb7917dd526aaff13a0850e3b02e3576bdd1dc90
Jack Francis <jackfrancis@gmail.com>
2015-10-07 18:08:51 -0700

    initial commit of goutils/osutils
    Containing the `Getenv(envVar string, defaultVal string)` function, a thin wrapper around `os.Getenv` but requiring a default string to return in case the environment variable is unset or is an empty string.

---
+++ osutils/getenv.go
@@ -0,0 +1,32 @@
+/*
+Copyright 2015 Jack Francis
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package osutil contains thin wrappers around built-in os packages
+package osutil
+
+import (
+	"os"
+)
+
+// Getenv passes along the 1st arg to os.Getenv and returns its value,
+// or a passed-in default (2nd arg) if os.Getenv returns an empty string
+func Getenv(envVar string, defaultVal string) string {
+	if val := os.Getenv(envVar); len(val) == 0 {
+		return defaultVal
+	} else {
+		return val
+	}
+}
---
+++ osutils/getenv_test.go
@@ -0,0 +1,38 @@
+/*
+Copyright 2015 Jack Francis
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package osutil
+
+import (
+	"os"
+	"testing"
+)
+
+func TestGetenv(t *testing.T) {
+	if err := os.Setenv("testEnvVar", "venkman"); err != nil {
+		t.Error("Something is wrong with the built-in os package!")
+	}
+
+	if testEnvVar := Getenv("testEnvVar", "default"); testEnvVar != "venkman" {
+		t.Error("Unable to get a known environment variable!")
+	}
+
+	os.Setenv("novelEnvVar", "") // make sure this is "unset" before testing for default
+	if novelEnvVar := Getenv("novelEnvVar", "spengler"); novelEnvVar != "spengler" {
+		t.Error("Didn't get the default passed-in string as expected!")
+	}
+
+}
